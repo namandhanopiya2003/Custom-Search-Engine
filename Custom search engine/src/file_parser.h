47108a27f7b604b92930dbe615c4879faa338d39
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-09-08 19:18:01 +0530

    Updated documentation

--- Custom search engine/src/file_parser.h
+++ Custom search engine/src/file_parser.h
@@ -1,14 +1,18 @@
 #ifndef FILE_PARSER_H
 #define FILE_PARSER_H
-
+// These are needed to use strings, lists, and maps in the code
 #include <string>
 #include <vector>
 #include <map>
 
+// This class helps read files and break text into words
 class FileParser {
 public:
+    // This function reads all files in a folder (It returns a map where each file is linked to its list of lines)
     std::map<std::string, std::vector<std::string>> parse_directory(const std::string& path);
+    // This function breaks a line of text into clean words (no symbols and all lowercase)
     std::vector<std::string> tokenize(const std::string& line);
 };
 
 #endif
+
